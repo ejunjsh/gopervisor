7d0e1af6756af63659b29451ec0ca64bf28c4c2b
zhouff <sjj050121014@163.com>
2017-07-09 14:33:51 +0800

    add ctl for gopervisor

--- gsctl/main.go
+++ gsctl/main.go
@@ -6,42 +6,65 @@ import (
 	"fmt"
 	"io/ioutil"
 	"encoding/json"
+	"regexp"
+	"strings"
 )
 
 func main(){
-	if len(os.Args)==2{
-		switch os.Args[1] {
-		case "status":
-			getResult("http://127.0.0.1:8081/p/status")
-			return
-		case "start":
-			getResult("http://127.0.0.1:8081/p/start")
-			return
-		case "restart":
-			getResult("http://127.0.0.1:8081/p/restart")
-			return
-		case "stop":
-			getResult("http://127.0.0.1:8081/p/stop")
-			return
+	if len(os.Args)==1 {
+	   fmt.Println("usage:<export remote=127.0.0.1:9443;> gsctl [start|restart|stop|status] <processname>")
+		fmt.Println("[...] is madatory, <...> is optional")
+
+	}
+    var address string
+
+	for _,env:=range os.Environ(){
+		if strings.Contains(env,"remote="){
+			address=strings.Split(env,"=")[1]
 		}
 	}
 
-	if len(os.Args)==3{
-
-		switch os.Args[1] {
-		case "status":
-			getResult("http://127.0.0.1:8081/p/"+os.Args[2]+"/status")
-			return
-		case "start":
-			getResult("http://127.0.0.1:8081/p/"+os.Args[2]+"/start")
-			return
-		case "restart":
-			getResult("http://127.0.0.1:8081/p/"+os.Args[2]+"/restart")
-			return
-		case "stop":
-			getResult("http://127.0.0.1:8081/p/"+os.Args[2]+"/stop")
-			return
+	if len(os.Args)>1{
+		if address==""{
+			address="localhost:9443"
+		}else if regexp.MustCompile(`:\d+`).Match([]byte(address)){
+			address="localhost"+address
 		}
+
+		if len(os.Args)==2 {
+			switch os.Args[1] {
+			case "status":
+				getResult("http://"+address+"/p/status")
+				return
+			case "start":
+				getResult("http://"+address+"/p/start")
+				return
+			case "restart":
+				getResult("http://"+address+"/p/restart")
+				return
+			case "stop":
+				getResult("http://"+address+"/p/stop")
+				return
+			}
+		}else {
+			switch os.Args[1] {
+			case "status":
+				getResult("http://"+address+"/p/"+os.Args[2]+"/status")
+				return
+			case "start":
+				getResult("http://"+address+"/p/"+os.Args[2]+"/start")
+				return
+			case "restart":
+				getResult("http://"+address+"/p/"+os.Args[2]+"/restart")
+				return
+			case "stop":
+				getResult("http://127.0.0.1:8081/p/"+os.Args[2]+"/stop")
+				return
+			}
+		}
+
+
+
 	}
 
 }
@@ -56,6 +79,7 @@ func getResult(url string){
 	res, err := http.Get(url)
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 
 	result, err := ioutil.ReadAll(res.Body)
@@ -63,6 +87,7 @@ func getResult(url string){
 
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 
 	var status []statusJson
