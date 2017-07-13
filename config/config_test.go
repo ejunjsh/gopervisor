b5a6ec2a3b0921451a25d7061c89682849cdcca1
zhouff <sjj050121014@163.com>
2017-07-14 01:23:46 +0800

    fix test synax error

--- config/config_test.go
+++ config/config_test.go
@@ -11,20 +11,20 @@ func TestLoadConfig(t *testing.T) {
 		t.Error(err)
 	}
 
-	if len(*n.Processes)!=2{
+	if len(n.Processes)!=2{
 		t.Error("wrong expectation.")
 	}
 
-	if (*n.Processes)[0].Env["env1"]!="env1"{
+	if (n.Processes)[0].Env["env1"]!="env1"{
 		t.Error("wrong env.")
 	}
 
-	if (*n.Processes)[0].Std.Errfile!="/opt/err"{
+	if (n.Processes)[0].Std.Errfile!="/opt/err"{
 		t.Error("wrong std.")
 	}
 
-    if 	(*n.Processes)[0].GetEnvStringArray()[0]!="env1=env1"{
+    if 	(n.Processes)[0].GetEnvStringArray()[0]!="env1=env1"{
 		t.Error("wrong env convertion.")
 	}
-
+    
 }
